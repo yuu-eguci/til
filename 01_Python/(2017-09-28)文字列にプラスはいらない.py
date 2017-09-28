15cc69b5cd0f5f486b9d6151449432f9e642adea
yuu-eguci <yuu.eguci@gmail.com>
2017-09-28 13:39:43 +0900

    master Add 文字列にプラスはいらない.py

---
+++ 01_Python/(2017-09-28)文字列にプラスはいらない.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+a = ('a' 'b'
+
+
+
+                                                                                                                'd'
+
+
+
+                                           'dsafasd'
+
+     'c')
+
+
+print(a)
