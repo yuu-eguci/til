8fb9b248cf4da8cf43659d6d58cf0dffeca5fef2
yuu-eguci <yuu.eguci@gmail.com>
2018-06-11 17:12:48 +0900

    master Add sys.path追加ノート.py

---
+++ 01_Python/(2018-06-11)sys.path追加ノート.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''sys.path 追加ノート
+
+print(sys.path) で表示されるパスのどこかに custom.pth ってファイルを作る。拡張子が pth なら名前はなんでもよい。
+
+中身に sys.path へ追加したいパスを書く。
+'''
+
+'''例
+
+# /Users/username/.pyenv/versions/3.6.0/lib/python3.6/site-packages/custom.pth
+/Users/username/python-my-modules
+'''
