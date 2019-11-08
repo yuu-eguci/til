0a2fdae6a40fa1c58f4c2b534903b7bb272f53b9
yuu-eguci <yuu.eguci@gmail.com>
2019-11-08 13:46:03 +0900

    Create (2019-11-08)lambdaの中で変数を使うときの罠.py

---
+++ 01_Python/(2019-11-08)lambdaの中で変数を使うときの罠.py
@@ -0,0 +1,35 @@
+"""lambda の中で変数を使うときの罠
+"""
+
+
+# あ,い,う,え,お って表示される。
+def function_a():
+    functions = []
+    functions.append(lambda: print('あ'))
+    functions.append(lambda: print('い'))
+    functions.append(lambda: print('う'))
+    functions.append(lambda: print('え'))
+    functions.append(lambda: print('お'))
+    for func in functions:
+        func()
+
+
+# お,お,お,お,お って表示される。
+def function_b():
+    functions = []
+    for hiragana in 'あいうえお':
+        functions.append(lambda: print(hiragana))
+        # lambda の中の hiragana は hiragana というグローバル変数の参照になっている。
+        # for 内の hiragana のコピーにはなっていない。
+        # つまり lambda: print(hiragana) は lambda: print('あ') にはなっておらず lambda: print(hiragana) のママなんだ。
+        # for を抜けたあとにグローバル変数 hiragana を参照したら「お」になるだろ? そういうことが起こっている。
+        #
+        # そこで lambda hiragana=hiragana: print(hiragana) って書くと解決する。
+        # こう書けば、 print(hiragana) の中の hiragana の参照先はグローバル変数の hiragana ではなく、 lambda の引数の hiragana になっている。
+        # hiragana=hiragana の右側の hiragana は for hiragana のコピーだから lambda hiragana='あ': print(hiragana) になるってわけ。
+    for func in functions:
+        func()
+
+
+function_a()
+function_b()
