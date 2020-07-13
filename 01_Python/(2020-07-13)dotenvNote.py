aaea64f938107eddbac2c3f443b4f7d39fe835a3
yuu-eguci <yuu.eguci@gmail.com>
2020-07-13 16:56:10 +0900

    Create (2020-07-13)dotenvNote.py

---
+++ 01_Python/(2020-07-13)dotenvNote.py
@@ -0,0 +1,41 @@
+"""
+dotenv Note
+
+Python で環境変数を取得するノート。
+waka-box で実践してる。
+"""
+
+
+class EnvNotFoundError(WakaBoxException):
+    """特定の環境変数が見つからないことを表す例外クラス。
+
+    Arguments:
+        WakaBoxException {[type]} -- [description]
+    """
+
+
+# .env で環境変数を取得する場合に対応します。
+# raise_error_if_not_found: .env が見つからなくてもエラーを起こさない。
+dotenv.load_dotenv(dotenv.find_dotenv(raise_error_if_not_found=False))
+
+
+def get_env(keyname: str) -> str:
+    """環境変数を取得します。
+
+    Arguments:
+        keyname {str} -- 環境変数名。
+
+    Raises:
+        EnvNotFoundError: 環境変数が見つからない。
+
+    Returns:
+        str -- 環境変数の値。
+    """
+    try:
+        # GitHub Actions では環境変数が設定されていなくても yaml 内で空文字列が入ってしまう。空欄チェックも行います。
+        _ = os.environ[keyname]
+        if not _:
+            raise KeyError(f'{keyname} is empty.')
+        return _
+    except KeyError as e:
+        raise EnvNotFoundError(keyname) from e
