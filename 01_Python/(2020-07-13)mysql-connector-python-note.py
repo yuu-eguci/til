431b4eefcee5eae19a732b7730de46c6cdda404f
yuu-eguci <yuu.eguci@gmail.com>
2020-11-08 11:34:22 +0900

    Update (2020-07-13)mysql-connector-python-note.py

--- 01_Python/(2020-07-13)mysql-connector-python-note.py
+++ 01_Python/(2020-07-13)mysql-connector-python-note.py
@@ -48,3 +48,93 @@ else:
 finally:
     # HACK: with ステートメントで書けるようにする。
     connection.close()
+
+
+#
+# ↓ with statement で
+#
+
+
+class DbClient:
+    """DB アクセスを行うクラスです。 with 構文で使用可能です。
+    """
+
+    def __enter__(self):
+        mysql_connection_config = {
+            'host': MYSQL_HOST,
+            'user': MYSQL_USER,
+            'password': MYSQL_PASSWORD,
+            'database': MYSQL_DATABASE,
+        }
+        self.connection = mysql.connector.connect(**mysql_connection_config)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
+
+    def find_target_images(self, limit: int = None) -> list:
+        """本プログラムの処理対象のレコードを HistoryFaceImage から取得します。
+
+        Returns:
+            list: FaceImage のリスト。
+        """
+
+        select_sql = ' '.join([
+            'SELECT',
+                'id,',  # noqa: E131
+                'createdAt,',
+                'imagePath',
+            'FROM historyfaceimage',
+            'WHERE',
+                'recognitionStatus = %s',
+            'ORDER BY id DESC',
+            f'LIMIT {limit}' if limit else '',
+        ])
+        cursor = self.connection.cursor(dictionary=True)
+        cursor.execute(select_sql, (WorkProgressStatus.WAITING,))
+        records = cursor.fetchall()
+        cursor.close()
+
+        # レコード状態ではなく、モデル状態で返します。
+        # NOTE: azure-cognitiveservices-vision-face がそういう作りだったため倣ってみます。
+        face_images = [FaceImage.from_history_face_image_record(_)
+                       for _ in records]
+
+        return face_images
+
+    def set_undetected_completed_status(
+            self, history_face_image_ids: list) -> None:
+        """顔が検出されなかったデータを更新します。
+        単に COMPLETED を付与するだけです。
+
+        Args:
+            history_face_image_ids (list): 更新対象の HistoryFaceImage.id。
+        """
+
+        if not history_face_image_ids:
+            return
+
+        # id 用のプレースホルダです。
+        placeholder = get_placeholder(len(history_face_image_ids))
+
+        # [COMPLETED, id, id, id, ...] です。
+        placeholder_values = [WorkProgressStatus.COMPLETED]
+        placeholder_values.extend(history_face_image_ids)
+
+        update_sql = ' '.join([
+            'UPDATE historyfaceimage',
+            'SET',
+                'recognitionStatus = %s,',  # noqa: E131
+                # FIXME: %H:%i:%s にすべき。
+                # NOTE: ただ mysql.connector のプレースホルダ %s とバッティングし使用できない……。
+                "updatedAt = DATE_FORMAT(NOW(), '%Y-%m-%dT%H:%i:00.000Z')",
+            f'WHERE id IN ({placeholder})',
+        ])
+        cursor = self.connection.cursor()
+        cursor.execute(update_sql, tuple(placeholder_values))
+        cursor.close()
+        self.connection.commit()
+
+
+with functions.DbClient() as db_client:
+    face_images = db_client.find_target_images()
