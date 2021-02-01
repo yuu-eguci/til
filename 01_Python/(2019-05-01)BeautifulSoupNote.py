d090c1f03e06e4ad5d3e72ea92674e7a74efa7ad
yuu-eguci <yuu.eguci@gmail.com>
2021-02-01 10:21:33 +0900

    Create (2019-05-01)BeautifulSoupNote.py

---
+++ 01_Python/(2019-05-01)BeautifulSoupNote.py
@@ -0,0 +1,23 @@
+
+"""BeautifulSoupNote
+
+pipenv install beautifulsoup4 lxml
+
+"""
+
+
+from bs4 import BeautifulSoup
+
+
+# requests.text 等で取得した文字列とする。
+text = ''
+
+# これでパース。
+soup = BeautifulSoup(text, 'lxml')
+
+# セレクタ指定で取得。これは CSS に慣れているぼくにはわかりやすい。
+ones = soup.select('#id-name')
+one = soup.select_one('#id-name')
+
+# 取得したタグから attribute を取得。これは JQuery に慣れているぼくにはわかりやすい。
+attr = one['data-price']
